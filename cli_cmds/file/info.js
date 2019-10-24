0536875778356583e0adb9d7b6bb3c315e29704f
James Cahill <jcahill773@gmail.com>
2019-10-24 19:29:35 +0100

    [File.Info] Add possible vs actual value checking

--- cli_cmds/file/info.js
+++ cli_cmds/file/info.js
@@ -60,15 +60,41 @@ exports.handler = argv => {
       );
       console.log(
         "Actual".cyan +
-          ` Server ID:${
+          ` Server ID: ${
             findOID(cert, "1.3.6.1.4.1.54622.0.1.3").toString().yellow
           }`
       );
       console.log(
-        `Certificate protocol:${
+        `Certificate protocol: ${
           findOID(cert, "1.3.6.1.4.1.54622.0.1.1").toString().yellow
         }`
       );
+
+      // Check the Possible vs Actual responses (User)
+      if (
+        data.id.toString().toLocaleLowerCase() ==
+        cert.cert.serialNumber.toString().toLocaleLowerCase()
+      ) {
+        console.log("User ID values match!".green);
+      } else {
+        console.log(
+          "User ID values do not match. This file has been tampered with.".red
+        );
+      }
+
+      // Check the Possible vs Actual responses (User)
+      if (
+        data.serverId.toString().toLocaleLowerCase() ==
+        findOID(cert, "1.3.6.1.4.1.54622.0.1.3").toLocaleLowerCase()
+      ) {
+        console.log("Server ID values match!".green);
+      } else {
+        console.log(
+          "Server ID values do not match. This file has been tampered with.".red
+        );
+      }
+
+      // End of command function
     } else {
       console.error("Unsupported file format, must be mdmc v3 or later.");
       return;
@@ -92,5 +118,11 @@ function validateJSON(body) {
 
 function findOID(certObj, oid) {
   const oidArr = certObj.cert.extensions;
-  return oidArr.find(i => i.id === oid).value;
+  return (
+    oidArr
+      .find(i => i.id === oid)
+      .value.toString()
+      // Three hex characters are part of our custom OIDs
+      .substring(3)
+  );
 }
