7803e9443ed7e1f7ad0ff08b0a9d6149f167d4a0
James Cahill <jcahill773@gmail.com>
2019-10-24 19:43:34 +0100

    Add [File.Export] function
    Closes #5.

---
+++ cli_cmds/file/export.js
@@ -0,0 +1,72 @@
+const fs = require("fs");
+require("colors");
+
+exports.command = "export <file> <exportPath>";
+exports.desc = "export the certificate from a mdmc file";
+
+exports.builder = yargs => {
+  yargs.positional("file", {
+    describe: "file to use",
+    type: "string"
+  });
+  yargs.positional("exportPath", {
+    describe: "path to export to",
+    type: "string"
+  });
+};
+
+// TODO: Consolidate the MDMC validation into a helper class, See #7/github.
+
+exports.handler = argv => {
+  // Check to make sure the file has a .mdmc extension
+  if (!argv.file.endsWith(".mdmc")) {
+    console.error("Invalid file extension, must be *.mdmc!");
+    return;
+  }
+
+  // Check to make sure the file exists
+  if (!fs.existsSync(argv.file)) {
+    console.error("File doesn't exist!");
+    return;
+  }
+
+  const fileData = fs.readFileSync(argv.file, "utf8");
+
+  // Check to see if the file is valid JSON.
+  const data = validateJSON(fileData);
+
+  if (data) {
+    // The data is valid JSON
+    if (data.version >= 3) {
+      // The file is probably a valid *.mdmc file
+      try {
+        // Add the pfx extension to the export path if it was not already present
+        if (!argv.exportPath.endsWith(".pfx")) argv.exportPath += ".pfx";
+        // Save the file
+        fs.writeFileSync(argv.exportPath, data.keypairs);
+        console.log("Exported! The password is blank.".green);
+      } catch {
+        console.error("Could not save the certificate.");
+      }
+
+      // End of command function
+    } else {
+      console.error("Unsupported file format, must be mdmc v3 or later.");
+      return;
+    }
+  } else {
+    console.error("File is not valid JSON!");
+    return;
+  }
+};
+
+function validateJSON(body) {
+  try {
+    var data = JSON.parse(body);
+    // if came to here, then valid
+    return data;
+  } catch (e) {
+    // failed to parse
+    return null;
+  }
+}
