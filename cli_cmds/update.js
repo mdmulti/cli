1ff68cc46f636597009d60e509262fb94a60c0bf
James Cahill <jcahill773@gmail.com>
2019-11-16 19:08:43 +0000

    Optional bleeding update check and commented debug

--- cli_cmds/update.js
+++ cli_cmds/update.js
@@ -2,14 +2,30 @@ const request = require("request");
 const cmpVer = require("compare-versions");
 const colors = require("colors");
 
-const REQ_URL = "https://registry.npmjs.org/mdmcli/latest";
+let REQ_URL = "https://registry.npmjs.org/mdmcli/latest";
+const REQ_URL_BLEEDING =
+  "https://raw.githubusercontent.com/mdmulti/cli/master/package.json";
 
 exports.command = "update";
 exports.desc = "check for updates";
 
+exports.builder = yargs => {
+  yargs.positional("source", {
+    choices: ["release", "bleeding"],
+    describe: "update source to check against",
+    default: "release"
+  });
+};
+
 exports.handler = argv => {
   const bef = new Date();
-  request(REQ_URL, (req, res) => {
+  let prefix = ""
+  if (argv.source == "bleeding") {
+      REQ_URL = REQ_URL_BLEEDING;
+      prefix = "Bleeding".magenta + " => ".gray 
+    }
+  //console.log(REQ_URL);
+  REQ_URL = request(REQ_URL, (req, res) => {
     const updateAvailable = cmpVer.compare(
       JSON.parse(res.body).version,
       require("../cli").version,
@@ -19,15 +35,15 @@ exports.handler = argv => {
 
     if (updateAvailable) {
       console.log(
-        `${"Update Available!".bgRed} Run '${
+        `${prefix}${"Update Available!".bgRed} Run '${
           "npm -i -g mdmcli@latest".cyan
         }' to install the latest version. ${took}`
       );
     } else {
-      console.log(`${colors.green("You are up-to-date!")} ${took}`);
+      console.log(`${prefix}${colors.green("You are up-to-date!")} ${took}`);
     }
 
-    console.log("NPM " + JSON.parse(res.body).version);
-    console.log("C " + require("../cli").version);
+    //console.log("NPM " + JSON.parse(res.body).version);
+    //console.log("C " + require("../cli").version);
   });
 };
