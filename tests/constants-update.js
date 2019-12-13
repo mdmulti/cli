a005f50091ec41cb3ebf44abe9aee9a8350a2108
James Cahill <jcahill773@gmail.com>
2019-12-13 18:41:36 +0000

    update command tests

--- tests/constants-update.js
+++ tests/constants-update.js
@@ -4,7 +4,7 @@ const { n, dynamic } = require("./common");
 
 // Configure chai
 chai.should();
-describe("Constants Update Command", () => {
+describe("Constants-Update Command", () => {
   dynamic("constants-update", cmd => {
     it(cmd, done => {
       n()
